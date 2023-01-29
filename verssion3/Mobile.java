17af16dca3bc4f9f1a4541f9d3658b9b36fb6358
Amzhm <123622028+Amzhm@users.noreply.github.com>
2023-01-29 19:40:10 +0100

    Add files via upload

---
+++ verssion3/Mobile.java
@@ -0,0 +1,50 @@
+
+
+public class Mobile extends Element {
+    private Configuration config;
+    private Position p;
+
+    public Mobile(Type t,Configuration c, Position p){
+        super(t);
+        config=c;
+        this.p=p;
+    }
+    public Position gePosition(){
+        return p;
+    }
+    public boolean setPosition(Position p){
+        if(config.estVide(p)){
+            this.p=p;
+            return true;
+        } 
+        return false;   
+    }
+    public Configuration getConfig(){
+        return config;
+    }
+    public void setConfig(Configuration c){
+        config=c;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Mobile other = (Mobile) obj;
+        if (config == null) {
+            if (other.config != null)
+                return false;
+        } else if (!config.equals(other.config))
+            return false;
+        if (p == null) {
+            if (other.p != null)
+                return false;
+        } else if (!p.equals(other.p))
+            return false;
+        return true;
+    }
+    
+}
